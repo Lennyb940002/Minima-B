69c2e4080270454977edf1534deb69a1a47f807e
lenny bouche <lennybouche@outlook.com>
2025-01-28 14:46:29 +0100

    first ct

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const connectDB = require('./utils/db');
 
 // Charger les variables d'environnement
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 3000;
-
 // Connexion à MongoDB
 connectDB();
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Configurer CORS
+app.use(cors({
+  origin: 'https://minima-front-e.vercel.app', // Remplacez par l'URL de votre frontend
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
+// Middleware pour parser le JSON
 app.use(express.json());
 
+// Définir une route simple pour vérifier le serveur
 app.get('/', (req, res) => {
   res.send('Hello, Minima Backend!');
 });
 
+// Importer et utiliser les routes pour les emails
 const emailRoutes = require('./routes/emails');
 app.use('/api/emails', emailRoutes);
 
+// Démarrer le serveur
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
